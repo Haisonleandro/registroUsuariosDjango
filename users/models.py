49154e9a6647947f9bea67b93cdcb07db90f595f
Haisonleandro <haisonleandrotoro@gmail.com>
2024-08-16 10:42:20 -0500

    se creo un modelo y se migro y registro en la base de datos

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+#new
+class persona(models.Model):
+    ESTADO_CHOICES = [
+        ('activo','Activo'),
+        ('inactivo', 'Inactivo')
+    ]
+    name = models.CharField(max_length=100)
+    cedula = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    estado = models.CharField(max_length=100,
+    choices=ESTADO_CHOICES, default='activo')
+    dataCreated = models.DateField(auto_now=True)
